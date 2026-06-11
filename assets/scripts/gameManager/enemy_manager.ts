2833423e651a2f4b0bd24da6291406568f2c0a31
XS <1578760644@qq.com>
2026-06-12 01:10:00 +0800

    通过极坐标方法在屏幕外随机生成敌人

--- assets/scripts/gameManager/enemy_manager.ts
+++ assets/scripts/gameManager/enemy_manager.ts
@@ -1,5 +1,6 @@
 import { _decorator, Component, instantiate, Node, NodePool, Prefab, Vec3 } from 'cc';
 import { Enemy_01 } from '../prefabs/enemy_01';
+import { GameManager } from './game_manager';
 const { ccclass, property } = _decorator;
 
 @ccclass('EnemyManager')
@@ -13,12 +14,12 @@ export class EnemyManager extends Component {
     @property({ displayName: '生成间隔，单位：秒' })
     public spawnInterval: number = 0.5;
 
-    @property(Node)
-    public spawnPos: Node = null; //暂时在这里获取生成位置，后面通过spawnManager调用生成方法来控制生成
     private _activeEnemies: Node[] = [];    //活跃敌人列表
 
     private _enemyPool: NodePool = null;
     private _spawnTimer: number = 0;
+
+    private _tempVec3: Vec3 = new Vec3(); //复用向量，主要用于计算随机生成敌人的位置
 
     private static _inst: EnemyManager;
     public static get inst(): EnemyManager {
@@ -52,7 +53,8 @@ export class EnemyManager extends Component {
         enemy.active = true;
         enemy.getComponent(Enemy_01).init();
         enemy.setParent(this.enemyParent);
-        const spawnPos = this.spawnPos.getWorldPosition();
+
+        const spawnPos = this.getRandomSpawnPosition(); //暂时在这里获取生成位置，后面通过spawnManager调用生成方法来控制生成
         enemy.setWorldPosition(spawnPos);
 
 
@@ -73,6 +75,32 @@ export class EnemyManager extends Component {
 
     public getActiveEnemies(): Node[] {
         return this._activeEnemies;
+    }
+
+    /* 
+    在玩家周围屏幕外区域随机生成一个世界坐标
+    @return 随机出生点 Vec3
+    */
+    private getRandomSpawnPosition(): Vec3 {
+        const player = GameManager.inst.player;
+        if (!player) return this._tempVec3.set(0, 0, 0); //安全处理
+
+        const playerPos = player.getWorldPosition();
+
+        // 1. 随机角度：0 到 2π
+        const angle = Math.random() * Math.PI * 2
+
+        //2.随机距离，740 ~ 800（确保在屏幕外）
+        const minDist = 740;
+        const maxDist = 800;
+        const distance = minDist + Math.random() * (maxDist - minDist);
+
+        // 3. 计算出生偏移
+        const offsetX = Math.cos(angle) * distance;
+        const offsetY = Math.sin(angle) * distance;
+
+        // 4. 写入复用向量，返回玩家位置 + 偏移
+        return this._tempVec3.set(playerPos.x + offsetX, playerPos.y + offsetY, 0)
     }
 }
 
