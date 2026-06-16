b4ddb41e4d0b84b952ba5ee312a6c1e5fdc9fb8f
XS <1578760644@qq.com>
2026-06-16 23:23:04 +0800

    feat(enemy)：新增碰撞检测功能 - enemy_base，添加checkCollision方法，用于与玩家进行碰撞检测

--- assets/scripts/base/enemy_base.ts
+++ assets/scripts/base/enemy_base.ts
@@ -1,6 +1,7 @@
-import { _decorator, Component, Node, Vec3 } from 'cc';
+import { _decorator, Component, Node, UITransform, Vec3 } from 'cc';
 import { GameManager } from '../gameManager/game_manager';
 import { EnemyManager } from '../gameManager/enemy_manager';
+import { Player } from '../prefabs/player';
 const { ccclass, property } = _decorator;
 
 @ccclass('EnemyBase')
@@ -12,18 +13,30 @@ export class EnemyBase extends Component {
     protected _isDead: boolean;
     protected _enemyType: string;
 
+    private _myTransfrom: UITransform = null;
+    private _attackCooldown: number = 0;
+
+    protected onLoad(): void {
+        this._myTransfrom = this.node.getComponent(UITransform);
+    }
+
     protected update(dt: number): void {
         if (this._isDead) return;
+        if (this._attackCooldown > 0) this._attackCooldown -= dt;
         this.updateMovement(dt);
     }
 
     updateMovement(dt: number) {
-        const playerPos = GameManager.inst.player.getWorldPosition();
+        const player = GameManager.inst.player;
+        if (!player) return;
+        const playerPos = player.getWorldPosition();
         const direction = this._tempVec3.set(playerPos)
             .subtract(this.node.getWorldPosition())
             .normalize();
         const offset = this._tempVec3.set(direction).multiplyScalar(dt * this._speed);
         this.node.setPosition(this.node.position.add(offset));
+
+        this.checkCollision();
     }
 
     init(hp: number = 1, speed: number = 400, type: string = '') {
@@ -43,6 +56,25 @@ export class EnemyBase extends Component {
         if (this._hp <= 0) {
             this._isDead = true;
             EnemyManager.inst.recycleEnemy(this.node, this._enemyType);
+        }
+    }
+
+    private checkCollision() {
+        if (this._attackCooldown > 0) return;
+        const player = GameManager.inst.player;
+        if (!player) return;
+
+        const playerComp = player.getComponent(Player);
+        if (!playerComp) return;
+
+        const enemyRadius = Math.min(this._myTransfrom.width, this._myTransfrom.height) * 0.5;
+        const playerRadius = playerComp.getHitRadius();
+
+        //当敌人与玩家的距离<敌人碰撞半径+玩家碰撞半径 触发碰撞
+        const totalDist = Vec3.distance(player.getWorldPosition(), this.node.getWorldPosition());
+        if (totalDist < enemyRadius + playerRadius) {
+            playerComp.takeDamage(1);
+            this._attackCooldown = 1;
         }
     }
 }
