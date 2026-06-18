50e55497151d9fc99522ae4c58e063720edb9b8d
XS <1578760644@qq.com>
2026-06-18 23:32:13 +0800

    chore(player):新增最大经验值

--- assets/config/player_config.ts
+++ assets/config/player_config.ts
@@ -1,6 +1,7 @@
-export const PLAYER_CONFIG ={
-    maxHp:4,       //最大血量
-    speed:600,      //移动速度 像塑/秒
+export const PLAYER_CONFIG = {
+    maxHp: 4,       //最大血量
+    speed: 600,      //移动速度 像塑/秒
+    maxEXP: 10,
 }
 
 
